1d4d5d3174397dc70e2a65bb4ba0dbb8de744a6d
Ciaran O'Neill <oneillci@gmail.com>
2013-10-18 22:39:17 +1100

    adding mef

--- ServiceRunner/Program.cs
+++ ServiceRunner/Program.cs
@@ -1,15 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ServiceRunner
 {
     class Program
     {
+        public Program()
+        {
+            var catalog = new AssemblyCatalog(typeof(Program).Assembly);
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+        }
+
         static void Main(string[] args)
         {
+            var p = new Program();
+        }
+    }
+
+  
+
+    public interface IJob
+    {
+        void Execute();
+    }
+
+    [Export(typeof(IJob))]
+    public class FirstJob : IJob
+    {
+        public void Execute()
+        {
+            Console.WriteLine("First job executing");
+        }
+    }
+    
+    [Export(typeof(IJob))]
+    public class SecondJob : IJob
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Second job executing");
         }
     }
 }
